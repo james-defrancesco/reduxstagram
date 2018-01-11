68bef4f89bd8afdfa90d593f6f7cb47c2acc4784
jimmysanfrancisco <jdefrancesco2@gmail.com>
2018-01-11 17:00:40 -0500

    Adds transition to "like" button / error likes undefined

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,10 +4,21 @@
 
 // when this first runs, state will be = to nothing, so we set it to an empty array
 function posts(state = [], action) {
-  console.log(state, action);
-    // return state because reducers take in state and modify it, then they have
-    // to return it
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+    console.log("Incrementing!");
+    const i = action.index;
+    return [
+      ...state.slice(0,i), //before the one we are updating
+      {...state[i], likes: state[i].likes + 1},
+      ...state.slice(i + 1), //after the one we are updating
+
+    ]
+    //return the updated state
+    default:
+      return state;
+  }
 
 }
- export default posts;
+
+export default posts;
